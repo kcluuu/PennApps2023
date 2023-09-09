ad686c813b95ae3cfbf5ff71b4e33919138f26be
Vivian Xiao <vivxia456@gmail.com>
2023-09-09 13:18:43 -0400

    ColorSelector in popup

--- popup.js
+++ popup.js
@@ -1,35 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("changeColorRed");
-    button.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-red" });
-      });
-    });
+  const buttonContainer = document.getElementById("buttonContainer");
+  const colorSelector = document.getElementById("colorSelector");
+  const selectedColorText = document.getElementById("selectedColor");
+
+  // Handle button clicks and color selection
+  buttonContainer.addEventListener("click", (event) => {
+    const button = event.target;
+    if (button.tagName === "BUTTON") {
+      const color = button.getAttribute("data-color");
+      if (color) {
+        // Update the selected color text
+        selectedColorText.textContent = `Selected Color: ${color}`;
+
+        // Set the color selector's value
+        colorSelector.value = `#${color}`;
+
+        // Update the color selector's appearance
+        colorSelector.style.background = `#${color}`;
+
+        // Send the color change message to the content script
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.tabs.sendMessage(tabs[0].id, { type: `change-color-${color}` });
+        });
+      }
+    }
   });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("changeColorBlue");
-    button.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-blue" });
-      });
-    });
+  // Handle color selection
+  colorSelector.addEventListener("input", () => {
+    const selectedColor = colorSelector.value;
+    selectedColorText.textContent = `Selected Color: ${selectedColor}`;
   });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("changeColorGreen");
-    button.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-green" });
-      });
-    });
-  });
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("changeColorYellow");
-    button.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-yellow" });
-      });
-    });
-  });
+  // Initialize the selected color text with the initial value
+  selectedColorText.textContent = `Selected Color: ${colorSelector.value}`;
+});
