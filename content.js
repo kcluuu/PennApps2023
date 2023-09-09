3bcea07f1cf6e9e13ada6a3b2a5b308a4e426a84
Vivian Xiao <vivxia456@gmail.com>
2023-09-08 23:57:32 -0400

    Added multiple color buttons

--- content.js
+++ content.js
@@ -1,5 +1,20 @@
 chrome.runtime.onMessage.addListener((message) => {
-    if (message.type == "change-color") {
-      document.body.style.backgroundColor = "yellow";
+    if (message.type == "change-color-red") {
+      document.body.style.backgroundColor = "red";
     }
+});
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type == "change-color-blue") {
+    document.body.style.backgroundColor = "blue";
+  }
+});
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type == "change-color-green") {
+    document.body.style.backgroundColor = "green";
+  }
+});
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type == "change-color-yellow") {
+    document.body.style.backgroundColor = "yellow";
+  }
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("change-color-button");
+    const button = document.getElementById("changeColorRed");
     button.addEventListener("click", () => {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color" });
+        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-red" });
+      });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("changeColorBlue");
+    button.addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-blue" });
+      });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("changeColorGreen");
+    button.addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-green" });
+      });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("changeColorYellow");
+    button.addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "change-color-yellow" });
       });
     });
   });
